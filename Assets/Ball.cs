4c5580e32f1d865157cb2b684737da6df33e07cc
sintaaryani15 <sintaaryani01@gmail.com>
2019-11-30 20:48:01 +0700

    menambahkan commit file

---
+++ Assets/Ball.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+	
+	[SerializeField]
+	float speed;
+	float radius;
+	Vector2 direction ;
+	
+	
+    // use this for initialization
+    void Start()
+    {
+        direction = Vector2.one.normalized;//direction is (1,1) normalLized
+		radius = transform.localScale.x / 2; //half the width
+		
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		transform.Translate (direction * speed * Time.deltaTime);
+		
+		//bounce of tom and bottom
+		if (transform.position.y < GameManager.bottomLeft.y + radius && direction.y < 0){
+			direction.y = -direction.y;
+		}
+		
+		if (transform.position.y > GameManager.topRight.y - radius && direction.y > 0){
+			direction.y = -direction.y;
+		}
+        //game over
+		
+		if (transform.position.x < GameManager.bottomLeft.x + radius && direction.x < 0){
+			
+			Debug.Log("Right player wins !!");
+			//for now, just freeze time
+		Time.timeScale = 0 ;
+		enabled = false ;//stop updating script
+			
+		}
+		
+				
+		if (transform.position.x > GameManager.topRight.x + radius && direction.x > 0){
+			Debug.Log("Left player wins !!");
+			//for now, just freeze time
+		Time.timeScale = 0 ;
+		enabled = false ; //stop updating script
+			
+			
+		
+		
+		
+		}
+    }
+
+	void OnTriggerEnter2D(Collider2D other) {
+		if (other.tag == "Paddle") {
+			bool isRight = other.GetComponent<Paddle>().isRight;
+			
+			
+			//if hitting right paddle and moving right,flip direction
+			if (isRight == true && direction.x > 0) {
+				direction.x = -direction.x ;
+				
+			}
+			//if hitting left paddle and moving right,flip direction
+			if (isRight == false && direction.x < 0) {
+				direction.x = -direction.x ;
+				
+			}
+	}
+	}
+}
+
+	
---
+++ Assets/Paddle.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+	
+	[SerializeField]
+	float speed;
+	float height;
+	
+	string input;
+	public bool isRight;
+	
+    // use this for initialization
+    void Start()
+    {
+		height = transform.localScale.y ;
+		
+        
+    }
+
+    
+	public void Init(bool isRightPaddle) {
+		
+		isRight = isRightPaddle;
+		Vector2 pos = Vector2.zero ;
+		
+			if (isRightPaddle) {
+				// place paddle on the right of screen
+				pos =  new Vector2(GameManager.topRight.x, 0);
+				pos -= Vector2.right * transform.localScale.x ; //move a bit  to the left
+			
+				input = "PaddleRight";
+			
+			}
+			
+			else {
+				// place paddle on the left of screen
+				pos =  new Vector2(GameManager.bottomLeft.x, 0);
+				pos += Vector2.right * transform.localScale.x ; //move a bit  to the right
+			
+				input = "PaddleLeft";
+			}
+	
+		//update this paddle's position
+		
+		transform.position = pos;
+		transform.name = input ;
+		
+	}
+	
+	
+	//update is called once per frame
+		void Update () {
+			// now lets move the paddle !
+			
+			//GetAxis is a number between -1 to 1 (-1 for down , 1 for up) 
+			float move = Input.GetAxis(input) * Time.deltaTime * speed ;
+			
+			//restrict paddle movement
+			//if paddle to low and user is continuing to move down, stop
+			if (transform.position.y < GameManager.bottomLeft.y + height / 2 && move < 0){
+				move = 0 ;
+			}
+			
+			//if paddle to high and user is continuing to move down, stop
+			if (transform.position.y > GameManager.topRight.y - height / 2 && move > 0){
+				move = 0 ;
+			}			
+			
+			transform.Translate (move * Vector2.up);
+		}
+			
+	
+}
